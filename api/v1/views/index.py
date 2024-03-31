cc547e71e94ef32b1e809020c7798f361bb96ec6
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-03-31 18:35:23 +0000

    ee

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,30 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     result = {
         "status": "OK"
+    }
+    return jsonify(result)
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Endpoint that retrieves the number of each objects by type"""
+    result = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
     return jsonify(result)
